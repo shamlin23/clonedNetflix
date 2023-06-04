174ed09170a8d0b8a3c26d581ab59f6dc8e0ff4a
shamlinNoushad <shamlin023@gmail.com>
2023-06-04 13:37:00 +0530

    Netflix final code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//netflix project
+//using axios
+import React from 'react'
+import NavBar from './Components/Navbar/NavBar'//automatically comes here/imported when component clled in code
+import "./App.css"
+import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/Rowpost/Rowpost';
+import { originals,action,comedy,romance,documentaries } from './urls';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar/>
+      <Banner/>
+      <Rowpost title="Netflix Originals" url={originals}/>
+      {/* working logic new way -dynamically*/}
+      <Rowpost title="Action" isSmall url={action}/>
+      <Rowpost title="Comedy" isSmall url={comedy}/>
+      <Rowpost title="Popular" isSmall url={documentaries}/>
+      <Rowpost title="Romance" isSmall url={romance}/>
     </div>
   );
 }
